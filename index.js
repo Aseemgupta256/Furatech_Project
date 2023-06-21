2c4e9c74aa4f4c7bf21c8925a379c1eab216a668
Aseem Gupta <500077426@stu.upes.ac.in>
2023-06-21 12:24:29 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+const port = 8080;
+app.use(express.json());
+app.use("/api/todos", require("./routes/todoRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
